f07db25af03d8182ba4ddd5d2a7e4f8833c292bd
Julio Diaz <>
2021-05-20 21:04:26 -0500

    Upate items amount on the cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,30 @@
+import React, {useRef, useState} from 'react';
+
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+    
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = parseInt(enteredAmount);
+
+        if(enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber)
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input label="Amount" input={{
+                ref: amountInputRef,
                 id: 'amount_' + props.id,
                 type: 'number',
                 max: '5',
@@ -13,6 +33,7 @@ const MealItemForm = props => {
                 defaultValue: '1'
             }}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     )
 }
